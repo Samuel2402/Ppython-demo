d63ef63591d02c5ecf43ebc0bd2befcee82bfb3e
Samuel <you@example.com>
2021-06-04 14:17:43 +0000

    added lists.py

---
+++ lists.py
@@ -0,0 +1,36 @@
+gen_1_pokemon = ['Pikachu', 'Charmander', 'Squirtle', 'Bulbasaur']
+gen_2_pokemon = ['Totadile', 'Cyndaquil', 'Chikorita']
+gen_3_pokemon = ['Mudkip', 'Treeko', 'Torchic']
+
+cool_pokemon = [gen_1_pokemon, gen_2_pokemon, gen_3_pokemon]
+
+#print(cool_pokemon[2][0])
+
+
+
+#print(pokemon[1])
+
+#pokemon[3] = "Pidgy"
+#print(pokemon[3])
+
+mudkip = ['name = Mudkip', ["3rd generation pokemon", "Evolves into Marshstomp"], 'Water type pokemon', 4]
+#print(mudkip[0])
+#print(mudkip[1])
+#print(mudkip[2])
+#print(mudkip[3])
+
+
+#mudkip.append("Final evolution is Swampert")
+#print(mudkip[4])
+
+#mudkip.remove("name = Mudkip")
+#print(mudkip)
+
+mudkip.reverse()
+print(mudkip)
+
+
+print(mudkip.count("Water type pokemon"))
+
+
+
