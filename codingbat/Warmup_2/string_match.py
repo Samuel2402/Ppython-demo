9fd7e843a33cecdc549b882b34604b5e7d40f9b4
Samuel <sam.castle02@gmail.com>
2021-06-09 21:10:24 +0000

    final commit 09/06/21

--- codingbat/Warmup_2/string_match.py
+++ codingbat/Warmup_2/string_match.py
@@ -1,1 +1,10 @@
 def string_match(a, b):
+    len_a = len(str(a))
+    len_b = len(str(b))
+    count = 0
+    for i in range(len_a, len_b):
+        if len_a[i] == len_b[i]:
+            count = count + 1
+
+    return count
+
